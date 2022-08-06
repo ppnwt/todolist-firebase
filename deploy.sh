5b6c5da8c88bd0a8953db67e58bd5ed96a1e9a65
New Panuwat <new_sbh@me.com>
2022-08-07 01:33:44 +0700

    tag v0.1.5

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-tagName="v0.1.4"
+tagName="v0.1.5"
 git add .
 git commit -m "tag $tagName"
 git tag $tagName -m "update tag deploy $tagName"
--- push.sh
+++ push.sh
@@ -1,18 +1,10 @@
 #!/bin/sh
 
 git add .
-msg="general message"
+msg="general commit message.."
 
 if [ $# -eq 1 ]; then msg="$1"; fi
 
 git commit -m "$msg"
 git pull --rebase
-
-build="BUILD"
-if [ $msg == *"$build"* ]; then
-  tagName="v0.1.4"
-  git tag $tagName -m "update tag deploy $tagName"
-  git push origin --tags
-else
-   git push origin main
-fi
+git push origin main
