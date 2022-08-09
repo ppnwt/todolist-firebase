79dc666d06849b3f08740ca3acafd434f5dce293
New Panuwat <new_sbh@me.com>
2022-08-09 17:19:47 +0700

    general commit message..

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-tagName="v0.1.8"
-git add .
-git commit -m "tag $tagName"
-git tag $tagName -m "update tag deploy $tagName"
-git push origin $tagName
+# tagName="v0.1.8"
+# git add .
+# git commit -m "tag $tagName"
+# git tag $tagName -m "update tag deploy $tagName"
+# git push origin $tagName
