186f413b69409c3b42181a8f6bc1cc711603810a
New Panuwat <new_sbh@me.com>
2022-11-29 17:20:52 +0700

    change rtdb firebase

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,13 +2,15 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAbTWgS2Fs7bBjFby57cnExArvas4MG8Z0",
-  authDomain: "new-pro-2caca.firebaseapp.com",
-  databaseUrl: "https://new-pro-2caca-default-rtdb.firebaseio.com/",
-  projectId: "new-pro-2caca",
-  storageBucket: "new-pro-2caca.appspot.com",
-  messagingSenderId: "1064283490598",
-  appId: "1:1064283490598:web:7b2cea3e3907360879d9e7",
+  apiKey: "AIzaSyDuCdn-6FDguf2AXNFhGoR2Pyi1UldhKbE",
+  authDomain: "todo-1-1caf2.firebaseapp.com",
+  databaseURL:
+    "https://todo-1-1caf2-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "todo-1-1caf2",
+  storageBucket: "todo-1-1caf2.appspot.com",
+  messagingSenderId: "219249354834",
+  appId: "1:219249354834:web:087d4c96db0a977877978b",
+  measurementId: "G-EHMLKV0YJS",
 };
 
 firebase.initializeApp(firebaseConfig);
